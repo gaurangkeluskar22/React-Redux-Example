f3617240efecf0883004c9ac9142ce26c406eca7
gaurangkeluskar22 <gaurangkeluskar22@gmail.com>
2023-06-27 23:29:11 +0530

    Added code for inc/dec

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container text-center mt-5">
+        <div className="row">
+          <div className="col-sm-12 mt-5">
+            <h1>Increment/Decrement Counter</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-12 mt-4">
+            <h3>React Redux Example</h3>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-4">
+           
+          </div>
+          <div className="col-sm-4 mt-5">
+          <button className="p-2 m-2">+</button>
+            <span className="p-2 m-2">0</span>
+            <button className="p-2 m-2">-</button>
+          </div>
+          <div className="col-sm-4">
+            
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
